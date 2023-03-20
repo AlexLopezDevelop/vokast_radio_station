95d23f5d7ec2edf51e506db94f4b7498ac6d2f6f
Alex López <32960226+AlexLopezDevelop@users.noreply.github.com>
2023-03-20 05:14:00 -0500

    added: new interface

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:vokast/models/base_model.dart';
 import 'package:http/http.dart' as http;
 
 class WebService {
   Future<BaseModel> getData(String url, BaseModel baseModel) async {
     final response = await http.get(Uri.parse(url), headers: {
-      'X-RapidAPI-Key': 'a0691df800mshff723f7acab4995p1d8626jsn83f1d693a6dd',
+      'X-RapidAPI-Key': '$dotenv.env["API_KEY"]',
       'X-RapidAPI-Host': 'radio-world-75-000-worldwide-fm-radio-stations.p.rapidapi.com'
     });
-
     if (response.statusCode == 200) {
 
       Map<String, dynamic> map = json.decode(response.body);
